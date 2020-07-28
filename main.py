7009aee22bc1803219b27c55c86f32437e3b4e70
Andy Eulogio <34007326+andygeek@users.noreply.github.com>
2020-07-28 14:15:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World with Flask'
+    return 'Hello World with Flaskkkkk'
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",debug=False)
